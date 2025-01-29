2907ebed9e4ae5995b15a045bf5e40128065fd54
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-01-29 15:21:57 +0100

    function to hide single post implemented

--- linkedin-team1/src/components/FeedAreaComp.jsx
+++ linkedin-team1/src/components/FeedAreaComp.jsx
@@ -3,6 +3,7 @@ import { Button, Spinner } from 'react-bootstrap';
 
 const FeedAreaComp = () => {
   const [posts, setPosts] = useState([]);
+  const [notShowArr, setNotShowArr] = useState([]);
 
   const getAllFeed = async () => {
     const url = 'https://striveschool-api.herokuapp.com/api/posts/';
@@ -57,6 +58,10 @@ const FeedAreaComp = () => {
     }
   };
 
+  const hidePost = (postId) => {
+    setNotShowArr(notShowArr.concat(postId));
+  };
+
   useEffect(() => {
     getAllFeed();
   }, []);
@@ -68,97 +73,105 @@ const FeedAreaComp = () => {
       ) : (
         <>
           {posts.splice(-50, posts.length).map((post) => {
-            return (
-              <div
-                className='bg-white rounded-4 mt-3 bordinoGames'
-                key={post._id}
-              >
-                <div className='d-flex justify-content-between align-items-center mx-3'>
-                  <p className=' text-secondary small'>Suggested</p>
-                  <div>
-                    <button className='btn bg-transparent'>
-                      <i className='bi bi-three-dots'></i>
-                    </button>
-                    <button className='btn bg-transparent'>
-                      <i className='bi bi-x-lg'></i>
-                    </button>
+            if (!notShowArr.includes(post._id)) {
+              return (
+                <div
+                  className='bg-white rounded-4 mt-3 bordinoGames'
+                  key={post._id}
+                >
+                  <div className='d-flex justify-content-between align-items-center mx-3'>
+                    <p className=' text-secondary small'>Suggested</p>
+                    <div>
+                      <button className='btn bg-transparent'>
+                        <i className='bi bi-three-dots'></i>
+                      </button>
+                      <button
+                        className='btn bg-transparent'
+                        onClick={() => {
+                          hidePost(post._id);
+                        }}
+                      >
+                        <i className='bi bi-x-lg'></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <hr className='mx-2' />
+
+                  {/* corpo principale del post */}
+                  <div className='mx-3'>
+                    {/* utente creatore post */}
+                    <div>
+                      <div className='d-flex justify-content-between'>
+                        <div>
+                          <img
+                            src={post.user.image}
+                            alt='user picture'
+                            className=' rounded-circle'
+                            width={'40px'}
+                            height={'40px'}
+                          />
+                        </div>
+                        <div className=' flex-grow-1 mx-2'>
+                          <h5>
+                            {post.user.name} {post.user.surname}
+                          </h5>
+                          <p>{post.user.title}</p>
+                          <p>
+                            {timePassed(post.updatedAt)} •
+                            <i className='bi bi-globe-americas ms-2'></i>
+                          </p>
+                        </div>
+                        <div>
+                          <button className='btn bg-transparent flex-grow-1'>
+                            <i className='bi bi-plus-lg me-2'></i>Follow
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* corpo del testo */}
+                    <div className='d-flex flex-column align-items-center my-2'>
+                      <p className='w-100 mb-2 overflow-x-hidden'>
+                        {post.text}
+                      </p>
+                      {post?.image && (
+                        <img src={post.image} alt='' className='w-75 mx-auto' />
+                      )}
+                    </div>
+
+                    {/* sezione commenti e reazioni */}
+                    <div className='d-flex justify-content-between align-items-center'>
+                      <div>
+                        <span className='text-secondary'>343</span>
+                      </div>
+                      <div>
+                        <Button variant='link'>20 comments</Button> •{' '}
+                        <Button variant='link'>44 reposts</Button>
+                      </div>
+                    </div>
+
+                    <hr className='my-1' />
+
+                    {/* bottoni interazioni col post */}
+                    <div className='d-flex justify-content-between mb-3'>
+                      <button className='btn bg-transparent fs-5'>
+                        <i className='bi bi-hand-thumbs-up-fill'></i> Like
+                      </button>
+                      <button className='btn bg-transparent fs-5'>
+                        <i className='bi bi-chat-dots'></i> Comment
+                      </button>
+                      <button className='btn bg-transparent fs-5'>
+                        <i className='bi bi-repeat'></i> Repost
+                      </button>
+                      <button className='btn bg-transparent fs-5'>
+                        <i className='bi bi-send-fill'></i>Send
+                      </button>
+                    </div>
                   </div>
                 </div>
-
-                <hr className='mx-2' />
-
-                {/* corpo principale del post */}
-                <div className='mx-3'>
-                  {/* utente creatore post */}
-                  <div>
-                    <div className='d-flex justify-content-between'>
-                      <div>
-                        <img
-                          src={post.user.image}
-                          alt='user picture'
-                          className=' rounded-circle'
-                          width={'40px'}
-                          height={'40px'}
-                        />
-                      </div>
-                      <div className=' flex-grow-1 mx-2'>
-                        <h5>
-                          {post.user.name} {post.user.surname}
-                        </h5>
-                        <p>{post.user.title}</p>
-                        {/* TODO funzione per calcolare il tempo trascorso dalla creazione del post */}
-                        <p>
-                          {timePassed(post.updatedAt)} •{' '}
-                          <i className='bi bi-globe-americas'></i>
-                        </p>
-                      </div>
-                      <div>
-                        <button className='btn bg-transparent flex-grow-1'>
-                          <i className='bi bi-plus-lg me-2'></i>Follow
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-
-                  {/* corpo del testo */}
-                  <div className='d-flex flex-column align-items-center my-2'>
-                    <p className='w-100 mb-2 overflow-x-hidden'>{post.text}</p>
-                    {post?.image && (
-                      <img src={post.image} alt='' className='w-75 mx-auto' />
-                    )}
-                  </div>
-
-                  {/* sezione commenti e reazioni */}
-                  <div className='d-flex justify-content-between align-items-center'>
-                    <div>
-                      <span className='text-secondary'>343</span>
-                    </div>
-                    <div>
-                      <Button variant='link'>20 comments</Button> •{' '}
-                      <Button variant='link'>44 reposts</Button>
-                    </div>
-                  </div>
-
-                  <hr className='my-1' />
-
-                  {/* bottoni interazioni col post */}
-                  <div className='d-flex justify-content-between mb-3'>
-                    <button className='btn bg-transparent fs-5'>
-                      <i className='bi bi-hand-thumbs-up-fill'></i> Like
-                    </button>
-                    <button className='btn bg-transparent fs-5'>
-                      <i className='bi bi-chat-dots'></i> Comment
-                    </button>
-                    <button className='btn bg-transparent fs-5'>
-                      <i className='bi bi-repeat'></i> Repost
-                    </button>
-                    <button className='btn bg-transparent fs-5'>
-                      <i className='bi bi-send-fill'></i>Send
-                    </button>
-                  </div>
-                </div>
-              </div>
-            );
+              );
+            }
           })}
         </>
       )}
