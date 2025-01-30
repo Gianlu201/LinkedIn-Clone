9f5de253697f5686ae53f8f755c0384fee1f0d48
camillazicari <camyzicari01@gmail.com>
2025-01-30 10:03:31 +0100

    organizzata pag jobs

--- linkedin-team1/src/components/HomeProfile.jsx
+++ linkedin-team1/src/components/HomeProfile.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable react/no-unescaped-entities */
 import { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
---
+++ linkedin-team1/src/components/JobsComponent.jsx
@@ -0,0 +1,14 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const JobsComponent = () => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} md={5} lg={4}></Col>
+        <Col xs={12} md={7} lg={8}></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default JobsComponent;
