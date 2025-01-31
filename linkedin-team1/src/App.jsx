bb92ee5c2940baebf69049a1d9821f41ac88b3d8
camillazicari <camyzicari01@gmail.com>
2025-01-31 09:43:11 +0100

    commit

--- linkedin-team1/src/App.jsx
+++ linkedin-team1/src/App.jsx
@@ -1,13 +1,13 @@
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import MainComponent from './components/MainComponent';
-import './App.css';
-import NavbarComponent from './components/NavbarComponent';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomeComponent from './components/HomeComponent';
-import JobsComponent from './components/JobsComponent';
-import ErrorPage from './components/ErrorPage';
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import MainComponent from "./components/MainComponent";
+import "./App.css";
+import NavbarComponent from "./components/NavbarComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeComponent from "./components/HomeComponent";
+import JobsComponent from "./components/JobsComponent";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
@@ -15,17 +15,17 @@ function App() {
       <div>
         <NavbarComponent />
         <Routes>
-          <Route path='/post' element={<HomeComponent />} />
-          <Route path='/post/:postId' element={<HomeComponent />} />
-          <Route path='/' element={<HomeComponent />} />
-          <Route path='/profile/:profileId' element={<MainComponent />} />
-          <Route path='/jobs' element={<JobsComponent />} />
-          <Route path='/jobs/:query' element={<JobsComponent />} />
+          <Route path="/post" element={<HomeComponent />} />
+          <Route path="/post/:postId" element={<HomeComponent />} />
+          <Route path="/" element={<HomeComponent />} />
+          <Route path="/profile/:profileId" element={<MainComponent />} />
+          <Route path="/jobs" element={<JobsComponent />} />
+          <Route path="/jobs/:query" element={<JobsComponent />} />
           <Route
-            path='/jobs/:query/company/:company/job/:jobId/:imgIndex'
+            path="/jobs/:query/company/:company/job/:jobId/:imgIndex"
             element={<JobsComponent />}
           />
-          <Route path={'/404'} element={<ErrorPage />} />
+          <Route path={"/*"} element={<ErrorPage />} />
         </Routes>
       </div>
     </BrowserRouter>
